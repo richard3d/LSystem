0a2025b585c51fdb0d4cd84b9c3114c1d3e17be1
Richard Becker <richard3d@gmail.com>
2018-05-15 12:25:42 -0700

    added scene to build settings

--- Assets/scripts/LSystem.cs
+++ Assets/scripts/LSystem.cs
@@ -17,6 +17,70 @@ public class Branch {
 	public Branch parent = null;
 	public List<Branch> children = new List<Branch>();
 	
+	//need rest angle relative to parent
+	//Vector3.Angle(dir, parent.dir) (nneds to be signed)
+	
+	//the current force acting on this node - (force relative to parent plus the sum of forces on this node's direct children)
+	// public void CalcForce() {
+		// return (-this.springConst * (this.angle - parent.angle - this.rest_angle) - this.dragConst(this.vel - parent.vel)) + CalcChildForces();
+	// }
+	
+	// public void CalcChildForces() {
+		// totalForce = 0;
+		// for(int i = 0; i < children.count; i++) {
+			// Branch child = children[i];
+			// totalForce += child.springConst(child.angle - this.angle - child.restAngle) + child.dragConst ( child.vel - this.vel);
+		// }
+		// return totalForce;
+	// }
+	
+	// public void CalcPartialDerivatives() {
+		// breadthFirst currNode in nodes
+		// if(currNode == this || currNode == this.parent
+           // ||currNode = any.children) {
+			   // calcDeriv();
+		   // }
+		// else{
+			// partialMatrix[currNode] = 0;
+		// }
+	// }
+	
+	// public void CalcQuaternionFrom1DSpaces(float x, float z) {
+		// float quat_ang = Mathf.Sqrt(x * x + z * z);
+		// Vector3 quat_axis = new Vector3(z, 0, -x);
+		// return Quaternion.AngleAxis(quat_ang, quat_axis);
+	// }
+	
+	// //calc relative angles in both dimensions
+	// rel_ang_x = abs_ang_x - parent.abs_ang_x;
+	// rel_ang_z = abs_ang_z - parent.abs_ang_z;
+	
+	// //calculate relative quaternions
+	// Quaternion rel_quat = CalcQuaternionFrom1DSpaces(rel_ang_x, rel_ang_z); 
+	// Quaternion dsct_quat_x = CalcQuaternionFrom1DSpaces(rel_ang_x+0.1f, rel_ang_z); 
+	// Quaternion dsct_quat_z = CalcQuaternionFrom1DSpaces(rel_ang_x, rel_ang_z+0.1f); 
+	
+	// //calculate world quaternions 
+	// Quaternion abs_quat = parent.abs_quat * rel_quat;
+	// Quaternion abs_dsct_quat_x = parent.abs_quat * dsct_quat_x;
+	// Quaternion abs_dsct_quat_z = parent.abs_quat * dsct_quat_z;
+	
+	// //calc the world vectors for figuring out the discrete partial derivatives
+	// Vector3 abs_dir = Vector3.up * abs_quat;
+	// Vector3 abs_dsct_x = Vector3.up * abs_dsct_quat_x;
+	// Vector3 abs_dsct_z = Vector3.up * abs_dsct_quat_z;
+	
+	// //calculate discrete partial derivative vectors that tell us how the current direction changes
+	// //based on small change in angle in the x or z direction
+	// Vector3 dsct_partial_x = abs_dsct_x - abs_dir;
+	// Vector3 dsct_partial_z = abs_dsct_z - abs_dir;
+	
+	// //iterate and calc correction vector
+	// Vector3 correction = coll.diff;
+	// scaledz = (correction - dsct_partial_x).project(dsct_partial_z);
+	// scaledx = (correction - scaledz).project(dsct_partial_x);
+	
+	
 	public void Draw() {
 		GL.Begin(GL.LINES);			
 		GL.Color(Color.red);
@@ -41,10 +105,12 @@ public class Branch {
 	}
 	
 	public void Grow() {
+		
 		if(length < max_length) {
 			length += Time.deltaTime*4;
 		} else {
 			for(int i = 0; i < children.Count; i++) {
+				//children[i].max_length *= 0.9f;
 				children[i].Grow();
 			}
 		}
@@ -226,6 +292,12 @@ public class LSystem : MonoBehaviour {
 				turtle.rot =  turtle.rot * rotation;
 			} else if(sentence[i] == '-') {
 				Quaternion rotation = Quaternion.Euler(0, 0, -Random.Range(min_angle, max_angle));
+				turtle.rot =  turtle.rot * rotation;
+			}  else if(sentence[i] == '<') {
+				Quaternion rotation = Quaternion.Euler(0, Random.Range(min_angle*4, max_angle*4), 0);
+				turtle.rot =  turtle.rot * rotation;
+			} else if(sentence[i] == '>') {
+				Quaternion rotation = Quaternion.Euler(0, -Random.Range(min_angle*4, max_angle*4), 0);
 				turtle.rot =  turtle.rot * rotation;
 			} else if(sentence[i] == '[') {
 				branch_stack.Push(turtle);
